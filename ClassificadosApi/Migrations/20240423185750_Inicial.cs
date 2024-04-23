bb89c6cd213b3910429216d8532a05fe0e6ff6fa
PedrovSilva <82241565+PedrovSilva@users.noreply.github.com>
2024-04-23 15:06:57 -0400

    Adicionar arquivos de projeto.

---
+++ ClassificadosApi/Migrations/20240423185750_Inicial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClassificadosApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Classificados",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    Data = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Classificados", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Classificados");
+        }
+    }
+}
---
+++ ClassificadosApi/Models/Classificado.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClassificadosApi.Models
+{
+    public class Classificado
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(80)]
+        public string Titulo { get; set; }
+        [Required]
+        [StringLength (250)]
+        public string Descricao { get; set; }
+        [Required]
+        public  DateTime Data { get; set; }
+        public Classificado() {
+            Data = DateTime.Today; // Preenche automaticamente com a data atual do sistema
+        }
+    }
+}
